f9803b0457677f75e0fa9f98e698bee2c7949bdd
FlorentLequibain <florent.lequibain@gmail.com>
2017-10-27 09:40:19 +0200

    add array of alls vehicules in IndexView.php

--- views/IndexView.php
+++ views/IndexView.php
@@ -18,16 +18,56 @@
      </head>
      <body>
 
- <!-- ~~~~~~~~~~~~~~~ HEADER ~~~~~~~~~~~~~~~ -->
- <div class="card">
+ <!-- ~~~~~~~~~~~~~~~ ARRAY WITH ALL VEHICULES ~~~~~~~~~~~~~~~ -->
+<div class="container">
 
- <?php
 
- foreach ($vehicules as $key => $vehicule) {
-   echo $vehicule['Id_vehicule'] . ' ' . $vehicule['Type'] . '</br>';
- }
+ <table class="table">
+   <thead class="thead-default">
+     <tr>
+       <th>Id</th>
+       <th>Type</th>
+       <th>Number of Wheels</th>
+       <th>Color</th>
+       <th>Price</th>
+       <th>Cabin Color</th>
+       <th>Luggage Rest Color</th>
+       <th>Actions</th>
+     </tr>
+   </thead>
 
- ?>
+   <tbody>
+
+     <?php
+     foreach ($vehicules as $key => $vehicule) {
+      ?>
+       <tr>
+       <th scope="row"><?php echo $vehicule['Id_vehicule'];  ?></th>
+       <td><?php echo $vehicule['Type'];  ?></td>
+       <td><?php echo $vehicule['Number_wheels'];  ?></td>
+       <td><?php echo $vehicule['Color'];  ?></td>
+       <td><?php echo $vehicule['Price'];  ?></td>
+       <td><?php echo $vehicule['Cabin_color'];  ?></td>
+       <td><?php echo $vehicule['Luggage_rest_color'];  ?></td>
+
+       <!-- BUTTONS SEE / MODIFY / DELETE -->
+       <td>
+         <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+           <button type="button" class="btn btn-secondary">Left</button>
+           <button type="button" class="btn btn-secondary">Middle</button>
+           <button type="button" class="btn btn-secondary">Right</button>
+         </div>
+       </td>
+       
+       </tr>
+
+       <?php
+     }
+     ?>
+
+   </tbody>
+ </table>
+
 </div>
 
          <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
